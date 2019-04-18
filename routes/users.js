e254d6fd659fea9b47e516758f6d8f442d9e900d
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-18 11:48:55 +0200

    Add more user routes

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,17 @@ router.get('/register', (req, res, next) => {
     res.send('Register');
 });
 
+router.get('/authenticate', (req, res, next) => {
+    res.send('Authenticate');
+});
+
+router.get('/profile', (req, res, next) => {
+    res.send('Profile');
+});
+
+router.get('/validate', (req, res, next) => {
+    res.send('Validate');
+});
+
+
 module.exports = router;
