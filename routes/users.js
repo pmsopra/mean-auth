4cd1c1328be03a777ca5c9360c407e185b587136
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-18 12:55:15 +0200

    Add user registration

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
+const User = require('../models/user');
+
 router.post('/register', (req, res, next) => {
-    res.send('Register');
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({ success: false, message: 'Failed to register user' });
+            return;
+        }
+
+        res.json({ success: true, message: 'User registered' });
+    });
 });
 
 router.get('/authenticate', (req, res, next) => {
