23f915efa4f1256a3c2bcdc65e0820edc689c335
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-18 10:37:15 +0200

    Add example user routing

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
+
+const users = require('./routes/users');
+
 const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/users', users);
 
 app.get('/', (req, res) => {
     res.send('Get accepted');
