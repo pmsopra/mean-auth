17f8bfc4c09969dfb52be906109aecbb06ac67c6
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-18 12:08:07 +0200

    Update users routing

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
     res.send('Register');
 });
 
@@ -12,10 +12,5 @@ router.get('/authenticate', (req, res, next) => {
 router.get('/profile', (req, res, next) => {
     res.send('Profile');
 });
-
-router.get('/validate', (req, res, next) => {
-    res.send('Validate');
-});
-
 
 module.exports = router;
