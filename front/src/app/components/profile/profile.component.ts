d42b24d2cc7c4f93da554e03b36e725c6c4dc4c4
P M <przemyslaw.musialik@soprasteria.com>
2019-04-20 07:56:53 +0200

    Add user profile interface

--- front/src/app/components/profile/profile.component.ts
+++ front/src/app/components/profile/profile.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
+interface Profile {
+  user: object;
+}
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -17,7 +21,7 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     this.authService.getProfile().subscribe(
-      profile => {
+      (profile: Profile)   => {
         this.user = profile.user;
       },
       err => {
